d6c91695ee0811771a605927de4fb690abaa6ebf
Samba SENE <devweb2301@gmail.com>
2023-03-01 14:15:08 +0100

    add
    Co-Authored-By: Sene <2642741+Sene@users.noreply.github.com>

--- Poo/robot/src/robot/Robot.java
+++ Poo/robot/src/robot/Robot.java
@@ -3,129 +3,120 @@ package robot;
 import java.util.Random;
 
 public class Robot {
-	private String nomRobot;
-	private boolean estAllumer;
-	private boolean peutSeDeplacer;
-	private boolean avecCargaison;
+    private String nomRobot;
+    private boolean estAllume;
+    private boolean peutSeDeplacer;
+    private boolean avecCargaison;
 
-	public enum Deplacement {
-		AVANT, ARRIERE, GAUCHE, DROITE;
-	}
+    public enum Deplacement {
+        AVANT, ARRIERE, GAUCHE, DROITE
+    }
 
-	public Robot(String _nomRobot, boolean _estAllumer, boolean _peutSeDeplacer, boolean _avecCargaison) {
-		this.nomRobot = _nomRobot;
-		this.estAllumer = _estAllumer;
-		this.peutSeDeplacer = _peutSeDeplacer;
-		this.avecCargaison = _avecCargaison;
-	}
+    public Robot(String nomRobot, boolean estAllume, boolean peutSeDeplacer, boolean avecCargaison) {
+        this.nomRobot = nomRobot;
+        this.estAllume = estAllume;
+        this.peutSeDeplacer = peutSeDeplacer;
+        this.avecCargaison = avecCargaison;
+    }
 
-	public int calculerDistance() {
-		Random rand = new Random();
+    public int calculerDistance() {
+        Random rand = new Random();
 
-		final int bMin = 0;
-		final int bMax = 100;
-		int distance = rand.nextInt(bMin - bMax + 1) + bMin;
-		return distance;
-	}
+        final int bMin = 0;
+        final int bMax = 100;
+        int distance = rand.nextInt(bMax - bMin + 1) + bMin;
+        return distance;
+    }
 
-	public String getname() {
-		return this.nomRobot;
-	}
+    public String getNomRobot() {
+        return this.nomRobot;
+    }
 
-	public void setname(String name) {
-		this.nomRobot = name;
-	}
+    public void setNomRobot(String nomRobot) {
+        this.nomRobot = nomRobot;
+    }
 
-	public boolean allumerRobot() {
-		if (!this.estAllumer) {
-			this.estAllumer = true;
-			return true;
-		} else {
-			return false;
-		}
+    public boolean allumerRobot() {
+        if (!this.estAllume) {
+            this.estAllume = true;
+            return true;
+        } else {
+            return false;
+        }
+    }
 
-	}
+    public boolean eteindreRobot() {
+        if (this.estAllume) {
+            this.estAllume = false;
+            return true;
+        } else {
+            return false;
+        }
+    }
 
-	public boolean eteindreRobot() {
-		if (this.estAllumer) {
-			this.estAllumer = false;
-			return true;
-		} else {
-			return false;
-		}
-	}
+    public boolean seDeplacer() {
+        if (!this.estAllume) {
+            return false;
+        } else {
+            int distance = calculerDistance();
 
-	public boolean seDeplacer() {
-		if (!this.estAllumer) {
-			return false;
-		} else {
-			int distance = calculerDistance();
+            if (distance > 10) {
+                this.peutSeDeplacer = true;
+                return true;
+            } else {
+                this.peutSeDeplacer = false;
+                return false;
+            }
+        }
+    }
 
-			if (distance > 10) {
-				this.peutSeDeplacer = true;
-				return true;
-			} else {
-				this.peutSeDeplacer = false;
-				return false;
-			}
-		}
+    public String deplacer(Deplacement deplacement) {
+        String message = "";
 
-	}
+        if (this.peutSeDeplacer && this.estAllume) {
+            switch (deplacement) {
+                case AVANT:
+                    message = "le robot avance";
+                    break;
+                case ARRIERE:
+                    message = "le robot recule";
+                    break;
+                case GAUCHE:
+                    message = "le robot tourne à gauche";
+                    break;
+                case DROITE:
+                    message = "le robot tourne à droite";
+                    break;
+            }
+        } else {
+            message = "Le robot ne peut pas se déplacer";
+        }
 
-	public String deplacer(Deplacement move) {
-		String str = "";
+        return message;
+    }
 
-		if (this.peutSeDeplacer && this.estAllumer) {
+    public boolean chargerCargaison() {
+        if (!this.estAllume && !this.avecCargaison) {
+            this.avecCargaison = true;
+            return true;
+        } else {
+            return false;
+        }
+    }
 
-			switch (move) {
-			case AVANT: {
-				str = "le robot avance";
-				break;
-			}
-			case ARRIERE: {
-				str = "le robot recule";
-				break;
-			}
-			case GAUCHE: {
-				str = "le robot tourne gauche";
-				break;
-			}
-			case DROITE: {
-				str = "le robot tourne droite";
-				break;
-			}
-			default: {
-				break;
-			}
+    public boolean dechargerCargaison() {
+        if (!this.estAllume && this.avecCargaison) {
+            this.avecCargaison = false;
+            return true;
+        } else {
+            return false;
+        }
+    }
 
-			}
-		} else {
-			return "Le robot ne peut pas se déplacer";
-		}
-		return str;
-	}
-
-	public boolean chargerCargaison() {
-		if (!this.estAllumer && this.avecCargaison) {
-			return false;
-		} else {
-			this.avecCargaison = true;
-			return true;
-		}
-	}
-
-	public boolean dechargerCargaison() {
-		if (!this.estAllumer && !this.avecCargaison) {
-			return false;
-		} else {
-			this.avecCargaison = false;
-			return true;
-		}
-	}
-
-	public String toString() {
-		return " Nom du robot :  " + nomRobot + "\n"+ " | alumer : " + estAllumer +  "\n" +" | etient : " + estAllumer + "\n"
-				+ " | se deplace: " + peutSeDeplacer +  "\n" +"| colis charger  :" + avecCargaison;
-	}
-
+    public String toString() {
+        return "Nom du robot : " + nomRobot + "\n" +
+                "Allumé : " + estAllume + "\n" +
+                "Peut se déplacer : " + peutSeDeplacer + "\n" +
+                "Avec cargaison : " + avecCargaison;
+    }
 }
